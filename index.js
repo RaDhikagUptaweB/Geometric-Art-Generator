3f6134e0d7f51384cd81197ad9f3ee92205d6686
Radhika gupta <67688493+RaDhikagUptaweB@users.noreply.github.com>
2024-04-08 21:21:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ const colors = ["#01d2fd", "#ffc700", "#fe9f12", "#06d0c7"];
 const boxes = document.querySelectorAll(".container div");
 let generatePattern = () => {
   boxes.forEach((box) => {
+    box.className = "";
+    let i = Math.floor(Math.random() * shapes.length);
+    let j = Math.floor(Math.random() * colors.length);
+    box.classList.add(shapes[i]);
+    box.style.backgroundColor = colors[j];
+  });
+};
+btn.addEventListener("click", generatePattern);
+window.addEventListener("load", generatePattern);
